f0927461734af1ac3e10f4e3a79a9d33fc2029d5
Jacob Abello <abello.jacob@gmail.com>
2013-06-10 15:19:38 +0800

    change bg color

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Simple PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>body {margin-top: 40px; background-color: #333;}</style>
+        <style>body {margin-top: 40px; background-color: #ffffff;}</style>
         <!-- <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet"> -->
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
